39551da9504b92cd12b704ea6684d7e6bee57e52
roheat2 <109902469+roheat2@users.noreply.github.com>
2022-07-24 18:17:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-string = "Odd-Even Finder"
+string = "To check whether the given number is odd or even."
 st.set_page_config(page_title=string, page_icon="🔍")
 
-st.title('Odd-Even Finder')
+st.title('To check whether the given number is odd or even.')
 x = st.number_input('Enter a number')
 if (x).is_integer():
-    if x%2==0:st.write("your number is even")
-    elif x%2!=0:st.write("your number is odd")
-else:st.write("please enter integer number")
+    if x%2==0:st.write("Even number.")
+    elif x%2!=0:st.write("Odd number")
+else:st.write("Enter valid integer")
